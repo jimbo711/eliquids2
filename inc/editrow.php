7a60c073409bcd3521cd1530a6ef209c9989ab8a
Jimbo <the.jim.farrugia@gmail.com>
2018-05-13 05:16:34 +1000

    Added validation to editrow.php.  Flavour select fields now include a blank first option.

--- inc/editrow.php
+++ inc/editrow.php
@@ -7,16 +7,36 @@ include 'connect.php';
 $name = $_GET['name'];
 $newQty = $_GET['newQty'];
 
-// Store query
-$sql = "UPDATE madeliquids
-        SET qty='$newQty' 
-        WHERE liquidname='$name'";
+// Create empty error message
+$errors = "";
 
-// Run Query
-if (mysqli_query($conn, $sql)) {
-    header('Location: ../index.php');
+// validate name field (only letters, dashes and spaces)
+if($name == ""){
+    $errors .= '<p>Blank field - You must choose a flavour update.</p>';
+}
+// vaildate qty (must be numeric and not empty)
+if (!is_numeric($newQty)) {
+    $errors .= '<p>Invalid qty - Qty must be a number.</p>';
+}
+
+// Continue if error message is still empty
+if ($errors == "") {
+    // Store query
+    $sql = "UPDATE madeliquids
+            SET qty='$newQty' 
+            WHERE liquidname='$name'";
+
+    // Run Query
+    if (mysqli_query($conn, $sql)) {
+        header('Location: ../index.php');
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
 } else {
-    echo "Error updating record: " . mysqli_error($conn);
+    // Else (there were errors), report and link home
+    echo '<p>Operation aborted due to errors.  See Below:</p>';
+    echo $errors;
+    echo '<p><a href="../index.php">Go Back...</a></p>';
 }
 
 ?>
--- inc/flavourfield.php
+++ inc/flavourfield.php
@@ -1,7 +1,11 @@
 <?php
 
+// Open select tag
 echo '<select name="name">';
-    
+
+// First option is blank
+echo '<option></option>';
+
 // Query all the liquid names
 $sql = "SELECT liquidname FROM madeliquids";
 $result = mysqli_query($conn, $sql) 
@@ -13,6 +17,7 @@ while ($row=mysqli_fetch_array($result)) {
     echo "<option>".$liquidname."</option>\r\n";
 }
 
+// Close select tag
 echo '</select>';
 
 ?>
