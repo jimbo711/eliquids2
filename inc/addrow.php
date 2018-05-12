11c0562ed07ec001e0ddd74f6c86cf4094557402
Jimbo <the.jim.farrugia@gmail.com>
2018-05-13 04:19:27 +1000

    Added validation and error handling to 'addrow.php'.  Added localhost to connect.php for testing.

--- inc/addrow.php
+++ inc/addrow.php
@@ -10,25 +10,46 @@ include 'connect.php';
 $newLiquidName = $_GET['name'];
 $newLiquidQty = $_GET['qty'];
 
-// change chars from html to equiv
-$newLiquidName = htmlspecialchars($newLiquidName);
-$newLiquidQty = htmlspecialchars($newLiquidQty);
+// Error checking vars
+$errors = "";
 
-// prevent sql injection
-$newLiquidName = mysqli_real_escape_string($conn, $newLiquidName);
-$newLiquidQty  = mysqli_real_escape_string($conn, $newLiquidQty);
-
-// store query in var
-$sql = 
-    // insert inputs into these columns
-    "INSERT INTO madeliquids (`liquidname`, `qty`) 
-     VALUES ('$newLiquidName', '$newLiquidQty')";
-
-// Run Query
-if (mysqli_query($conn, $sql)) {
-    header('Location: ../index.php');
-} else {
-    echo "Error updating record: " . mysqli_error($conn);
+// validate name field (only letters, dashes and spaces)
+if(!preg_match("/^[a-zA-Z\'\-\040\.]+$/", $newLiquidName)){
+    $errors .= '<p>Invalid name - Only letters, dashes and spaces are allowed.</p>';
+}
+// vaildate qty (must be numeric and not empty)
+if (!is_numeric($newLiquidQty)) {
+    $errors .= '<p>Invalid qty - Qty must be a number.</p>';
 }
 
+// Continue if there are no errors
+if ($errors == "") {
+
+    // change chars from html to equiv
+    $newLiquidName = htmlspecialchars($newLiquidName);
+    $newLiquidQty = htmlspecialchars($newLiquidQty);
+
+    // prevent sql injection
+    $newLiquidName = mysqli_real_escape_string($conn, $newLiquidName);
+    $newLiquidQty  = mysqli_real_escape_string($conn, $newLiquidQty);
+
+    // store query in var
+    $sql = 
+        // insert inputs into these columns
+        "INSERT INTO madeliquids (`liquidname`, `qty`) 
+        VALUES ('$newLiquidName', '$newLiquidQty')";
+
+    // Run Query
+    if (mysqli_query($conn, $sql)) {
+        header('Location: ../index.php');
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
+
+} else {
+    // Else (there were errors), report and link home
+    echo '<p>Operation aborted due to errors.  See Below:</p>';
+    echo $errors;
+    echo '<p><a href="../index.php">Go Back...</a></p>';
+}
 ?>
--- inc/connect.php
+++ inc/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-/***********************************
-    Make Connection to Database
-***********************************/
-
+/*********************************************
+    Make Connection to infinityfree Database
+**********************************************/
+/*
 // Database server connection (host, username, pwd)
 $conn = mysqli_connect("sql104.epizy.com", "epiz_22072025", "zJYnznjozQww");
 
@@ -20,5 +20,27 @@ if (mysqli_connect_errno()) {
 
 // Select the DB to work with
 mysqli_select_db($conn, "epiz_22072025_eliquid") or die(mysqli_error($conn));
+ */
 
+/******************************************
+    Make Connection to localhost Database
+*******************************************/
+
+// Database server connection (host, username, pwd)
+$conn = mysqli_connect("localhost", "root", "");
+
+// If connection isn't returning true
+if (!$conn) {
+    // present error
+    die("Error connecting to database: ".mysqli_connect_error());
+}
+// Check for errors
+if (mysqli_connect_errno()) {
+    // present error
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+// Select the DB to work with
+mysqli_select_db($conn, "eliquid") or die(mysqli_error($conn));
+ 
 ?>
