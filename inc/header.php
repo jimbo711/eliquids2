96caea04712c7129ca55a701f060a36b2cc6daca
Jimbo <the.jim.farrugia@gmail.com>
2018-05-19 22:03:59 +1000

    styled navbar and improved header include

--- inc/header.php
+++ inc/header.php
@@ -2,7 +2,7 @@
 // Default header variables
 if (!isset($page_title)) {
     // If page title isn't set, set to default
-    $page_title = "eLiquids";
+    $page_title = "eLiquid Data";
 }
 if (!isset($path_home)) {
     // If path to root isn't set, set to default
@@ -22,3 +22,37 @@ if (!isset($path_home)) {
     <script src="<?php echo $path_home; ?>js/neworder.js"></script>
 </head>
 <body>
+
+    <nav class="navbar fixed-top navbar-expand navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo $path_home; ?>index.php">Eliquid Data</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>  
+    </nav>
--- login.php
+++ login.php
@@ -1,9 +1,32 @@
 <?php
 // Assign page title
 $page_title = "Login";
-// Page header
-require_once 'inc/header.php';
+if (!isset($path_home)) {
+    // If path to root isn't set, set to default
+    $path_home = ""; // Assign "../" on other pages where necessary
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $page_title; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?php echo $path_home; ?>styles/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="<?php echo $path_home; ?>js/neworder.js"></script>
+    <style type="text/css">
+    html, body {height: 100%;}
+    .container:nth-child(2),
+    .container .row.justify-content-center.align-items-center {
+        height: 100%;
+        min-height: 100%;
+    }
+    </style>
+</head>
+<body>
 
 <div class="container h-100">
     <div class="row h-100 justify-content-center align-items-center">
