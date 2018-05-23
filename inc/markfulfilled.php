386ddb7678da209d29d7f5e8bd309b4b5bfbcb73
Jimbo <the.jim.farrugia@gmail.com>
2018-05-23 21:37:41 +1000

    Customer name is now automatically prepended to address in 'change order' section.

--- inc/markfulfilled.php
+++ inc/markfulfilled.php
@@ -139,7 +139,7 @@ require_once 'header.php';
                                         <th>Address</th>
                                         <td>
                                             <div class="input-group">
-                                                <textarea class="form-control" rows="4" id="address" name="address"><?php echo str_replace(", ","\r\n",$row['address']); ?></textarea>
+                                                <textarea class="form-control" rows="4" id="address" name="address"><?php echo $row['name']."\r\n".str_replace(", ","\r\n",$row['address']); ?></textarea>
                                                 <div class="input-group-append">
                                                     <button id="copyaddress" type="button" class="btn btn-outline-secondary" onclick="autocopy('address', 'copyaddress')">
                                                         Copy
